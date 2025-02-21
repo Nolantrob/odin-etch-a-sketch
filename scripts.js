35a571415c7aecb6bfb5a815dee02f642626743e
Nolantrob <nolanrobertson97@gmail.com>
2025-02-21 18:14:19 -0500

    Unify all click event listeners into one switch case statement

--- scripts.js
+++ scripts.js
@@ -2,44 +2,57 @@ const etchContainer = document.querySelector('#etch-container');
 const createGridButton = document.querySelector('.create-grid-button');
 const clearGridButton = document.querySelector('.clear-grid-button');
 
-let gridCount = 16;
+generateGrid(100);
 
-// Combine all event listeners later
-createGridButton.addEventListener('click', () => {
-    gridCount = prompt('Enter desired grid size:');
-    generateGrid();
-})
-
-clearGridButton.addEventListener('click', () => {
-    const allBoxes = document.querySelectorAll('.square-div');
-    allBoxes.forEach(box => {
-        box.classList.remove('colored-in');
-    })
-})
 
 document.querySelector('.delete-grid-button').addEventListener('click', () => {
     etchContainer.innerHTML = '';
 })
 
-// const buttons = document.querySelectorAll('button');
-// buttons.forEach(button => {
-//     button.addEventListener('click', () => {
+// All button click event listeners
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        switch (button.id) {
+            case 'delete-button':
+                etchContainer.innerHTML = '';
+                break;
+            case 'clear-button':
+                const allBoxes = document.querySelectorAll('.square-div');
+                allBoxes.forEach(box => {
+                    box.classList.remove('colored-in');
+                })
+                break;
+            case 'create-button':
+                gridCount = prompt('Enter desired grid size:');
+                deleteCurrentGrid();
+                generateGrid(gridCount);
+                break;
+            default:
+                break;
+        }
+    })
+})
 
-//     })
-// })
 
+function generateGrid(gridSideLength) {
 
-function generateGrid() {
+    for (let i = 0; i < gridSideLength; i++) {
 
-    for (let i = 0; i < gridCount; i++) {
-
-        for (let r = 0; r < gridCount; r++) {
+        for (let r = 0; r < gridSideLength; r++) {
             const squareDiv = document.createElement('div');
             squareDiv.classList.add('square-div');
 
-            squareDiv.addEventListener('mouseenter', () => {
-                squareDiv.classList.add('colored-in');
+
+            squareDiv.addEventListener('mouseover', (event) => {
+                if (event.ctrlKey === true) {
+                    squareDiv.classList.add('colored-in');
+                }
+                if (event.altKey === true) {
+                    squareDiv.classList.remove('colored-in');
+                }
             })
+
             etchContainer.appendChild(squareDiv);
         }
 
@@ -47,8 +60,6 @@ function generateGrid() {
 
 
 }
-
-generateGrid();
 
 function deleteCurrentGrid() {
     etchContainer.innerHTML = '';
