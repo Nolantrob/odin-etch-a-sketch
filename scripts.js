ffc6ff4e3f455a435054320d1dbb524c328547a7
Nolantrob <nolanrobertson97@gmail.com>
2025-02-22 21:20:08 -0500

    Allow dynamic creation of boards based on user input

--- scripts.js
+++ scripts.js
@@ -2,12 +2,17 @@ const etchContainer = document.querySelector('#etch-container');
 const createGridButton = document.querySelector('.create-grid-button');
 const clearGridButton = document.querySelector('.clear-grid-button');
 
+// By default, open a 100x100 grid
 generateGrid(100);
 
+function promptUserForInput() {
+    gridCount = prompt('Enter desired grid size:');
+    generateGrid(gridCount);
+}
 
-document.querySelector('.delete-grid-button').addEventListener('click', () => {
+function deleteCurrentGrid() {
     etchContainer.innerHTML = '';
-})
+}
 
 // All button click event listeners
 const buttons = document.querySelectorAll('button');
@@ -25,7 +30,6 @@ buttons.forEach(button => {
                 break;
             case 'create-button':
                 gridCount = prompt('Enter desired grid size:');
-                deleteCurrentGrid();
                 generateGrid(gridCount);
                 break;
             default:
@@ -34,17 +38,37 @@ buttons.forEach(button => {
     })
 })
 
-
+// Generate a grid based on the user's input number.
+// EXAMPLE: An input of 50 produces a 50x50 grid
 function generateGrid(gridSideLength) {
+    if (gridSideLength > 100){
+        alert('Maximum Allowable Size 100')
+        return;
+    }
 
-    for (let i = 0; i < gridSideLength; i++) {
+    deleteCurrentGrid();
 
-        for (let r = 0; r < gridSideLength; r++) {
+    // let gridLineBreakHandler = document.createElement('style');
+    let gridLineBreakHandler = document.querySelector('.grid-line-break-handler');
+
+    gridLineBreakHandler.textContent = `
+        .square-div:nth-child(${gridSideLength}n + 1) {
+            width: 100%;
+            border: 0;
+            height: 0;
+        }`
+
+    for (let columns = 0; columns < gridSideLength; columns++) {
+
+        for (let rows = 0; rows < gridSideLength; rows++) {
             const squareDiv = document.createElement('div');
             squareDiv.classList.add('square-div');
 
-
             squareDiv.addEventListener('mouseover', (event) => {
+                // Stops crash if both keys are held down
+                if(event.ctrlKey === true && event.altKey === true){
+                    return;
+                }
                 if (event.ctrlKey === true) {
                     squareDiv.classList.add('colored-in');
                 }
@@ -59,8 +83,4 @@ function generateGrid(gridSideLength) {
     }
 
 
-}
-
-function deleteCurrentGrid() {
-    etchContainer.innerHTML = '';
 }
